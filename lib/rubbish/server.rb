06df82eca9e2ee779a7dc58e7a186804b9006581
byelipk <byelipk@gmail.com>
2015-11-30 22:18:14 -0500

    [FEATURE] echo server

--- lib/rubbish/server.rb
+++ lib/rubbish/server.rb
@@ -24,9 +24,27 @@ module Rubbish
     private
 
       def handle_client(client)
+        # We need to read from the client buffer
+        # in order to process commands.
+        header = client.gets.to_s
+
+        return unless header.start_with? "*"
+
+        num_args = header[1..-1].to_i
+
+        cmd = num_args.times.map do
+          len = client.gets[1..-1].to_i
+          client.read(len + 2).chomp
+        end
+
+        response = case cmd[0].downcase
+        when "ping" then "+PONG\r\n"
+        when "echo" then "$#{cmd[1].length}\r\n#{cmd[1]}\r\n"
+        end
+
         # Now we can communicate to the client through
         # the client socket.
-        client.write("+PONG\r\n")
+        client.write(response)
       ensure
         client.close
       end
--- test/acceptance/skeleton_test.rb
+++ test/acceptance/skeleton_test.rb
@@ -2,11 +2,19 @@ require_relative '../test_helper'
 
 class SkeletonTest < Minitest::Test
 
+  parallelize_me!
+
   include AcceptanceHelper
 
   def test_server_responds_to_ping
     with_server do
       assert_equal "PONG", client.ping
+    end
+  end
+
+  def test_it_responds_to_echo
+    with_server do
+      assert_equal "hello", client.echo("hello")
     end
   end
 end
