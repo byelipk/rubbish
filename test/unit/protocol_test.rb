166a88606498fb26394528decebfc4b1fe8221a3
byelipk <byelipk@gmail.com>
2015-12-01 21:31:32 -0500

    add scaffolding for a protocol test

---
+++ test/unit/protocol_test.rb
@@ -0,0 +1,17 @@
+require_relative '../test_helper'
+
+class ProtocolTest < Minitest::Test
+  def self.it_marshals(ruby, wire)
+    test_name = ruby.to_s
+    test_name.downcase!
+    test_name.gsub!(/ /, '_')
+
+    define_method "test_#{test_name}" do
+      assert_equal wire, Rubbish::Protocol.marshal(ruby)
+    end
+  end
+
+  it_marshals :ok,     "+OK\r\n"
+  it_marshals nil,     "$-1\r\n"
+  it_marshals "Hello", "$5\r\nHello\r\n"
+end
