cfbc0b9af4d545b5cdf3af5632b5d0b26e2bc2ab
byelipk <byelipk@gmail.com>
2015-12-01 23:34:44 -0500

    wrap acceptance tests inside module

--- test/acceptance/getter_and_setter_test.rb
+++ test/acceptance/getter_and_setter_test.rb
@@ -1,17 +1,17 @@
 require_relative '../test_helper'
 
-class GetterAndSetterTest < Minitest::Test
+module Acceptance
+  class GetterAndSetterTest < Minitest::Test
+    parallelize_me!
 
-  parallelize_me!
+    include AcceptanceHelper
 
-  include AcceptanceHelper
-
-  def test_it_gets_and_sets_values
-    with_server do
-      assert_equal nil,   client.get('abc')
-      assert_equal "OK",  client.set('abc', '123')
-      assert_equal "123", client.get('abc')
+    def test_it_gets_and_sets_values
+      with_server do
+        assert_equal nil,   client.get('abc')
+        assert_equal "OK",  client.set('abc', '123')
+        assert_equal "123", client.get('abc')
+      end
     end
   end
-
 end
--- test/acceptance/skeleton_test.rb
+++ test/acceptance/skeleton_test.rb
@@ -1,33 +1,34 @@
 require_relative '../test_helper'
 
-class SkeletonTest < Minitest::Test
+module Acceptance
+  class SkeletonTest < Minitest::Test
+    parallelize_me!
 
-  parallelize_me!
+    include AcceptanceHelper
 
-  include AcceptanceHelper
-
-  def test_server_responds_to_ping
-    with_server do
-      c = client
-      c.without_reconnect do
-        assert_equal "PONG", c.ping
-        assert_equal "PONG", c.ping
-        assert_equal "PONG", c.ping
+    def test_server_responds_to_ping
+      with_server do
+        c = client
+        c.without_reconnect do
+          assert_equal "PONG", c.ping
+          assert_equal "PONG", c.ping
+          assert_equal "PONG", c.ping
+        end
       end
     end
-  end
 
-  def test_it_responds_to_echo
-    with_server do
-      assert_equal "hello", client.echo("hello")
+    def test_it_responds_to_echo
+      with_server do
+        assert_equal "hello", client.echo("hello")
+      end
     end
-  end
 
-  def test_it_supports_multiple_client_connections
-    with_server do
-      assert_equal "hello", client.echo("hello")
-      assert_equal "hello", client.echo("hello")
-      assert_equal "hello", client.echo("hello")
+    def test_it_supports_multiple_client_connections
+      with_server do
+        assert_equal "hello", client.echo("hello")
+        assert_equal "hello", client.echo("hello")
+        assert_equal "hello", client.echo("hello")
+      end
     end
   end
 end
