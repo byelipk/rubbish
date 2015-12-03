aabd600d1464d9e7ba1ac577486de01612b49b6a
byelipk <byelipk@gmail.com>
2015-12-03 09:06:13 -0500

    remove spaces

--- test/acceptance/hash_test.rb
+++ test/acceptance/hash_test.rb
@@ -8,8 +8,8 @@ module Acceptance
 
     def test_it_supports_hashes
       with_server do
-        assert_equal "OK",   client.hset('myhash', 'abc', '123'), "Expected 123 to be OK"
-        assert_equal "OK",   client.hset('myhash', 'def', '456'), "Expected 456 to be OK"
+        assert_equal "OK", client.hset('myhash', 'abc', '123'), "Expected 123 to be OK"
+        assert_equal "OK", client.hset('myhash', 'def', '456'), "Expected 456 to be OK"
 
         assert_equal ['123', '456'], client.hmget('myhash', 'abc', 'def')
       end
