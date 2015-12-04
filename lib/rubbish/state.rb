c2399643e6beb7365747deac0fc036418d8d9929
byelipk <byelipk@gmail.com>
2015-12-03 21:37:41 -0500

    logarithmic perf on set operations

--- lib/rubbish/state.rb
+++ lib/rubbish/state.rb
@@ -179,25 +179,56 @@ module Rubbish
   end
 
   class ZSet
+    attr_reader :entries_to_score, :sorted_by_score
+
     def initialize
-      @entries = []
+      @entries_to_score = Hash.new
+      @sorted_by_score  = Array.new
     end
 
     def add(score, member)
-      @entries << [score, member]
-      @entries.sort!
+      entries_to_score[member] = score
+      elem  = [score, member]
+      index = bsearch_index(sorted_by_score, elem)
+      sorted_by_score.insert(index, elem)
     end
 
     def range(start, stop)
-      @entries[start..stop].map {|x| x[1]}
+      sorted_by_score[start..stop].map {|x| x[1]}
     end
 
     def rank(member)
-      @entries.index {|x| x[1] == member}
+      score = entries_to_score[member]
+
+      return unless score
+
+      bsearch_index(sorted_by_score, [score, member])
     end
 
     def score(member)
-      @entries.detect {|x| x[1] == member}[0]
+      entries_to_score[member]
+    end
+
+    def bsearch_index(ary, x)
+      return 0 if ary.empty?
+
+      low  = 0
+      high = ary.length - 1
+
+      while high >= low
+        idx  = low + (high - low) / 2
+        comp = ary[idx] <=> x # 1, 0, -1
+
+        if comp == 0
+          return idx
+        elsif comp > 0
+          high = idx - 1
+        else
+          low = idx + 1
+        end
+      end
+
+      idx + (comp < 0 ? 1 : 0)
     end
   end
 end
