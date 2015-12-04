de1aa1563392e3ee7c17a08759f20acf624af646
byelipk <byelipk@gmail.com>
2015-12-04 09:34:12 -0500

    basic implementation of transactions work

--- lib/rubbish/client.rb
+++ lib/rubbish/client.rb
@@ -15,6 +15,11 @@ module Rubbish
     def initialize(socket)
       @socket = socket
       @buffer = String.new
+      @tx_buf = nil
+    end
+
+    def in_tx?
+      @tx_buf
     end
 
     def process!(state)
@@ -27,10 +32,17 @@ module Rubbish
       @buffer = buffer[processed..-1]
 
       cmds.each do |cmd|
-        response = case cmd[0].downcase
-        when "ping"  then :pong
-        when "echo"  then cmd[1]
-        else state.apply_command(cmd)
+        response = if in_tx?
+          case cmd[0].downcase
+          when "exec" then
+            result  = @tx_buf.map {|c| dispatch(state, c)}
+            @tx_buf = nil
+            result
+          else
+            @tx_buf << cmd; :queued
+          end
+        else
+          dispatch(state, cmd)
         end
 
         # Now we can communicate to the client through
@@ -45,6 +57,15 @@ module Rubbish
 
       def unmarshaler
         Unmarshaler.new
+      end
+
+      def dispatch(state, cmd)
+        case cmd[0].downcase
+        when "ping"  then :pong
+        when "echo"  then cmd[1]
+        when "multi" then @tx_buf = []; :ok
+        else state.apply_command(cmd)
+        end
       end
 
   end
