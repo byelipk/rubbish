7ba32c90d0a97c65c3708de1e213303b645db427
byelipk <byelipk@gmail.com>
2015-12-04 10:42:01 -0500

    refactor out zset

--- lib/rubbish/state.rb
+++ lib/rubbish/state.rb
@@ -1,5 +1,6 @@
 require 'set'
 require_relative './clock'
+require_relative './zset'
 
 module Rubbish
   class State
@@ -150,7 +151,7 @@ module Rubbish
       value.add(score, member)
 
       touch!(key)
-      
+
       1
     end
 
@@ -200,59 +201,5 @@ module Rubbish
       ws.each(&:call)
     end
 
-  end
-
-  class ZSet
-    attr_reader :entries_to_score, :sorted_by_score
-
-    def initialize
-      @entries_to_score = Hash.new
-      @sorted_by_score  = Array.new
-    end
-
-    def add(score, member)
-      entries_to_score[member] = score
-      elem  = [score, member]
-      index = bsearch_index(sorted_by_score, elem)
-      sorted_by_score.insert(index, elem)
-    end
-
-    def range(start, stop)
-      sorted_by_score[start..stop].map {|x| x[1]}
-    end
-
-    def rank(member)
-      score = entries_to_score[member]
-
-      return unless score
-
-      bsearch_index(sorted_by_score, [score, member])
-    end
-
-    def score(member)
-      entries_to_score[member]
-    end
-
-    def bsearch_index(ary, x)
-      return 0 if ary.empty?
-
-      low  = 0
-      high = ary.length - 1
-
-      while high >= low
-        idx  = low + (high - low) / 2
-        comp = ary[idx] <=> x # 1, 0, -1
-
-        if comp == 0
-          return idx
-        elsif comp > 0
-          high = idx - 1
-        else
-          low = idx + 1
-        end
-      end
-
-      idx + (comp < 0 ? 1 : 0)
-    end
   end
 end
